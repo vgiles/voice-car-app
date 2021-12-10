bdac902c9322895310cfbc7a06276ce4e5562a97
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-12-10 20:24:06 +1100

    Fixed it

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 let img;
-var radius = 150;
+var radius = 115;
 var angle = 0;
 var speed = 0.05;
+let defaultAngle = 120;
+let sensitivity = 1.5;
 // var steps = TWO_PI/360;
 
 
 function setup() {  
-    let cnv = createCanvas(800, 600);
+    let cnv = createCanvas(windowWidth, windowHeight);
     cnv.mousePressed(userStartAudio);
     // createCanvas(400, 400);
     mic = new p5.AudioIn();
     mic.start();
+    angleMode(DEGREES);  
   }
 
 function draw() {
@@ -28,23 +31,33 @@ function moveDial() {
     let cx = width/2;
     let cy = height/2;
     // background(0);
-    translate(0, 0); //set the new origin/point of rotation
-    
-    // strokeWeight(3);
-    // stroke(255);
-    // fill(255);
-    if (micLevel > 0.003) {
-        line(cx, cy, cx + cos(angle) * radius, cy + sin(angle) * radius);
-        rotate(angle);
-        angle = angle + (micLevel*5);
+    translate(cx, cy); //set the new origin/point of rotation
+    strokeWeight(10);
+    stroke(255, 140, 130, 100);
+    // rotate(angle);
+    // line(0, 0, 1 * radius, 1 * radius);
+    if (micLevel > 0.005) {
+        angle = (angle + 1) * (micLevel*sensitivity);
+        rotate(defaultAngle + angle);
+        line(0, 0, 1 * radius, 1 * radius);
     } else {
-        angle = angle - (micLevel*5);
-        rotate(angle);
-        // angle = 0;
-        line(cx, cy, cx + cos(angle) * radius, cx + sin(angle) * radius);
-        
-        
+        angle = (angle - 1) * micLevel;
+        rotate(defaultAngle + angle);
+        line(0, 0, 1 * radius, 1 * radius);
     }
+//     if (micLevel > 0.02) {
+//         line(cx, cy, cx + cos(angle) * radius, cy + sin(angle) * radius);
+//         rotate(angle);
+//         angle = angle + (micLevel*5);
+//     } else {
+//         angle = angle - (micLevel*5);
+//         rotate(angle);
+//         // angle = 0;
+//         line(cx, cy, cx + cos(angle) * radius, cx + sin(angle) * radius);
+        
+        
+//     }
+    // angle = angle + 5;
 }
 
 function preload() {
